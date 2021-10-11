660ae42df25f5d3b31ee032f0784857bc257b9ea
Your Namedeniskoblya <denis.koblya@gmail.com>
2021-10-11 17:32:02 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,18 +47,6 @@ for (let index = 0; index < tabs.length; index++) {
 }
 
 
-
-// $("ul.tabs__caption").on("click", "li:not(.active)", function () {
-//     $(this)
-//         .addClass("active")
-//         .siblings()
-//         .removeClass("active")
-//         .closest("div.tabs")
-//         .find("div.tabs__content")
-//         .removeClass("active")
-//         .eq($(this).index())
-//         .addClass("active");
-// });
 // конец скрипт табов //
 
 // ---------------------------------------------------------------------
